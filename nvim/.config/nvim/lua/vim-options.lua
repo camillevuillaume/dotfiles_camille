dbdb8fbc5b16283dca86a344cedab04297d03d01
Camille Vuillaume <camillevuillaume@gmail.com>
2025-07-14 19:07:15 +0900

    added power menu in waybar

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,5 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set textwidth=0")
+vim.cmd("set wrapmargin=0")
+vim.cmd("set wrap")
+vim.cmd("set linebreak")
 vim.g.mapleader = " "
 vim.wo.number = true
