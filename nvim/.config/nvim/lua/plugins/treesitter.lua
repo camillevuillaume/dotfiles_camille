1721d84a2a214c631dcf022c117944be2fb6eba1
Camille Vuillaume <camillevuillaume@gmail.com>
2025-09-17 22:55:59 +0900

    updated hyprland

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate', -- Ensures parsers are updated on plugin updates
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'c', 'lua', 'python' }, -- Specify languages to install parsers for
+      highlight = {
+        enable = true, additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      -- Add other configurations as needed
+    }
+  end
+}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -21,6 +21,7 @@ alias yayfzf="yay -Slq | fzf --multi --preview 'yay -Si {1}' | xargs -ro sudo ya
 #alias yayfzf="yay -Slq | fzf -q "$1" --preview 'yay -Si {1}'| xargs -ro yay -S"
 alias grep='grep --color=auto'
 alias ls="eza --long --git --icons --hyperlink"
+alias ssh='TERM="xterm-256color" kitty +kitten ssh'
 
 fastfetch --logo .config/fastfetch/arch.gif --logo-type kitty-direct --logo-animate true --logo-width 22
 
