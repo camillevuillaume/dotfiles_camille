837e4920fdad112bfaac9f97ef1488050299f229
Camille Vuillaume <camillevuillaume@gmail.com>
2025-09-21 15:52:32 +0900

    fixed multi host script on second machine

--- hyprland/.config/hypr/scripts/init_config.sh
+++ hyprland/.config/hypr/scripts/init_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm ~/dotfiles_camille/hyprland/.config/hypr/custom-by-hostname.conf &> /dev/null
-ln -s "./custom-$HOSTNAME.conf" ~/dotfiles_camille/hyprland/.config/hypr/custom-by-hostname.conf &> /dev/null
+#rm ~/dotfiles_camille/hyprland/.config/hypr/custom-by-hostname.conf &> /dev/null
+ln -sf "./custom-$HOSTNAME.conf" ~/dotfiles_camille/hyprland/.config/hypr/custom-by-hostname.conf &> /dev/null
 
