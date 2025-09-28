5fa00dc3e6692c619e443eee9cda8086adf220f9
Camille Vuillaume <camillevuillaume@gmail.com>
2025-09-28 13:41:55 +0900

    customized swayidle by host

---
+++ swayidle/.config/swayidle/swayidle_init_by_host.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# swayidle configuration for multiple hosts
+
+# Get the hostname
+HOST1="ajax"
+HOST2="achilles"
+CONFIG_DIR="$HOME/.config/swayidle"
+
+
+# Define idle parameters for host 1
+if [ "$HOSTNAME" = "$HOST1" ]; then
+  swayidle -w -C $CONFIG_DIR/swayidle_$(HOST1).conf
+  #echo $CONFIG_DIR/swayidle_${HOST1}.conf
+
+# Define idle parameters for host2
+elif [ "$HOSTNAME" = "$HOST2" ]; then
+  swayidle -w -C $CONFIG_DIR/swayidle_$(HOST2).conf
+  #echo $CONFIG_DIR/swayidle_${HOST2}.conf
+fi
+
