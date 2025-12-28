e2e29a088518327c6312d1e3ba61f49dd25afb84
Camille Vuillaume <camillevuillaume@gmail.com>
2025-12-28 16:25:14 +0900

    added barbar

---
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -0,0 +1,24 @@
+return {
+  "romgrk/barbar.nvim",
+  dependencies = {
+    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  },
+  init = function() vim.g.barbar_auto_setup = false end,
+  opts = {
+    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    -- animation = true,
+    -- insert_at_start = true,
+    -- …etc.
+  },
+  keys = {
+    -- Navigate between buffers
+    { "<A-h>", "<Cmd>BufferPrevious<CR>", desc = "Previous buffer" },
+    { "<A-l>", "<Cmd>BufferNext<CR>", desc = "Next buffer" },
+    -- Close buffer
+    { "<A-c>", "<Cmd>BufferClose<CR>", desc = "Close buffer" },
+    -- Find buffer
+    { "<A-b>", "<Cmd>BufferPick<CR>", desc = "Pick buffer" },
+  },
+}
+
