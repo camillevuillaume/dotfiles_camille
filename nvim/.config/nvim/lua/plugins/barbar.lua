a3ec97a08c05c142ed9a1edea39d4f44da3da310
Camille Vuillaume <camillevuillaume@gmail.com>
2025-12-29 08:23:36 +0900

    barbar config updated

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -7,8 +7,8 @@ return {
   init = function() vim.g.barbar_auto_setup = false end,
   opts = {
     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
-    -- insert_at_start = true,
+    animation = true,
+    insert_at_end = true,
     -- …etc.
   },
   keys = {
