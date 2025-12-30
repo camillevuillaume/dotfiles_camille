ca83689a101058270f5c49ccefa0cb7c5061ecc0
Camille Vuillaume <camillevuillaume@gmail.com>
2025-12-30 10:17:44 +0900

    Added weather info

---
+++ waybar/.config/waybar/waywttr.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# A simple script to fetch and display weather information for a specified location using wttr.in
+
+
+weather_bar() {
+  curl "wttr.in/${LOCATION}?format=1" 2>/dev/null
+}
+
+
+weather_popup() {
+  curl "wttr.in/${LOCATION}?3nF" 2>/dev/null
+  read -s -n 1
+}
+
+
+get_config() {
+  if [ -f "./waywttr.conf" ]; then
+    source "./waywttr.conf"
+  elif [[ -f "$HOME/.config/waywttr.conf" ]]; then
+    source "$HOME/.config/waywttr.conf"
+  elif [[ -f "$HOME/.config/waybar/waywttr.conf" ]]; then
+    source "$HOME/.config/waybar/waywttr.conf"
+  else
+    #Use default values
+    LOCATION="Paris"
+  fi
+}
+
+
+# barsymbol("Tokyo")
+get_config
+if [ "$1" == "bar" ]; then
+  weather_bar
+elif [ "$1" == "popup" ]; then
+  weather_popup
+else
+  echo "Usage: $0 [bar|popup]"
+  exit 1
+fi
+exit 0
