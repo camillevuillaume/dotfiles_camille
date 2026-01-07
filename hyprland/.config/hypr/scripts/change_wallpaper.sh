9570effa5bcb280cfd34cfd9da802b78f3e18c46
Camille Vuillaume <camillevuillaume@gmail.com>
2026-01-07 18:16:42 +0900

    fixed hyprland wallpaper scripts

--- hyprland/.config/hypr/scripts/change_wallpaper.sh
+++ hyprland/.config/hypr/scripts/change_wallpaper.sh
@@ -21,35 +21,30 @@ SHADE4_WALLPAPER=~/.config/hypr/wallpapers/Fuji-shade4.jpg
 # Calculate the present hour and save it to a variable 
 PRESENT_TIME=$(date +%H)
 
-echo "Current time: $PRESENT_TIME"
-
-# In an if loop, check if current time is between 6 am and 6 pm 
-# and assign day wallpaper to wallpaper variable.
-# Else, if time is between 6 pm and 6 am, 
-# assign night time wallpaper to wallpaper variable.
-if [ $PRESENT_TIME -ge 9 ] && [ $PRESENT_TIME -lt 16 ]; then
+# Assign wallpaper depending on time
+if [ "$PRESENT_TIME" -ge 9 ] && [ "$PRESENT_TIME" -lt 16 ]; then
 	WALLPAPER=$DAY_WALLPAPER
-elif [ $PRESENT_TIME -ge 16 ] && [ $PRESENT_TIME -lt 17 ]; then
+elif [ "$PRESENT_TIME" -ge 16 ] && [ "$PRESENT_TIME" -lt 17 ]; then
 	WALLPAPER=$SHADE1_WALLPAPER
-elif [ $PRESENT_TIME -ge 17 ] && [ $PRESENT_TIME -lt 18 ]; then
-  WALLPAPER=$SHADE2_WALLPAPER
-elif [ $PRESENT_TIME -ge 18 ] && [ $PRESENT_TIME -lt 19 ]; then
-  WALLPAPER=$SHADE3_WALLPAPER
-elif [ $PRESENT_TIME -ge 19 ] && [ $PRESENT_TIME -lt 20 ]; then
-  WALLPAPER=$SHADE4_WALLPAPER
-elif [ $PRESENT_TIME -ge 5 ] && [ $PRESENT_TIME -lt 6 ]; then
+elif [ "$PRESENT_TIME" -ge 17 ] && [ "$PRESENT_TIME" -lt 18 ]; then
+	WALLPAPER=$SHADE2_WALLPAPER
+elif [ "$PRESENT_TIME" -ge 18 ] && [ "$PRESENT_TIME" -lt 19 ]; then
+	WALLPAPER=$SHADE3_WALLPAPER
+elif [ "$PRESENT_TIME" -ge 19 ] && [ "$PRESENT_TIME" -lt 20 ]; then
 	WALLPAPER=$SHADE4_WALLPAPER
-elif [ $PRESENT_TIME -ge 6 ] && [ $PRESENT_TIME -lt 7 ]; then
-  WALLPAPER=$SHADE3_WALLPAPER
-elif [ $PRESENT_TIME -ge 7 ] && [ $PRESENT_TIME -lt 8 ]; then
-  WALLPAPER=$SHADE2_WALLPAPER
-elif [ $PRESENT_TIME -ge 8 ] && [ $PRESENT_TIME -lt 9 ]; then
-  WALLPAPER=$SHADE1_WALLPAPER
-else	
+elif [ "$PRESENT_TIME" -ge 5 ] && [ "$PRESENT_TIME" -lt 6 ]; then
+	WALLPAPER=$SHADE4_WALLPAPER
+elif [ "$PRESENT_TIME" -ge 6 ] && [ "$PRESENT_TIME" -lt 7 ]; then
+	WALLPAPER=$SHADE3_WALLPAPER
+elif [ "$PRESENT_TIME" -ge 7 ] && [ "$PRESENT_TIME" -lt 8 ]; then
+	WALLPAPER=$SHADE2_WALLPAPER
+elif [ "$PRESENT_TIME" -ge 8 ] && [ "$PRESENT_TIME" -lt 9 ]; then
+	WALLPAPER=$SHADE1_WALLPAPER
+else
 	WALLPAPER=$NIGHT_WALLPAPER
 fi
 
-echo "Applying wallpaper: $WALLPAPER"
+# echo "Applying wallpaper: $WALLPAPER"
 
 # Apply the wallpaper to current display.
 if pgrep -x niri > /dev/null; then
@@ -58,8 +53,7 @@ else
   if ! pgrep -x hyprpaper > /dev/null; then
     exit 0
   fi
-  hyprctl hyprpaper reload ,"$WALLPAPER"
-  hyprctl hyprpaper unload unused
+	hyprctl hyprpaper wallpaper ",$WALLPAPER"
 fi
 # Exit with success.
 exit 0
