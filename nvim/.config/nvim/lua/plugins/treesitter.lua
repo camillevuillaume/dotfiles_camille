bd693793f4f8ca25a41bf6e3429dd35f635e7f54
Camille Vuillaume <camillevuillaume@gmail.com>
2026-05-02 21:34:37 +0900

    fixed nvim config and added packages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   build = ':TSUpdate', -- Ensures parsers are updated on plugin updates
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'c', 'lua', 'python', 'javascript', 'yaml' }, -- Specify languages to install parsers for
-      highlight = {
-        enable = true, additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      -- Add other configurations as needed
-    }
-  end
+  opts =   {
+    ensure_installed = { 'c', 'lua', 'python', 'javascript', 'yaml', 'markdown' },
+  },
 }
